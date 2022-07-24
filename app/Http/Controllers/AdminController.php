b6a4563abcad7d7fbe1e90793afc0a64dbc6b6c5
Thuanncoi2002 <79820101+thuannddph13844@users.noreply.github.com>
2022-07-24 21:24:31 +0700

    commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //
+    private $v;
+    public function __construct()
+    {
+        $this->v = [];
+    }
+    public function add_danh_muc($method, $parameters)
+    {
+
+    }
+}
